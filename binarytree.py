c060b0abc1c95370e93e1c9d99eb1898749ac6af
larmor freq <100037095+cwlg102@users.noreply.github.com>
2022-05-26 14:34:12 +0900

    Add files via upload

--- binarytree.py
+++ binarytree.py
@@ -57,34 +57,48 @@ class BinaryTree:
             #해당 노드의 정보를 가지고 있어서 지워지지 않는것으로 생각됨... 
             #따라서 조금 번거롭지만 다음과 같은 방식으로 해봤음
             delflag = 0
-            if node.left:
-                if node.left.left == None and node.left.right == None:
-                    node.left = None
-                    delflag = 1
-                if not delflag:    #여기서 continue를 걸어버리면 오른쪽 확인을 못함
+            if node.left: #왼쪽 자식 존재할 시
+                if node.left.left == None and node.left.right == None: #왼쪽자식의 왼쪽이 없고 왼쪽자식의 오른쪽이없다면
+                    node.left = None #왼쪽자식이 리프노드이므로, 왼쪽 자식 삭제
+                    delflag = 1 #삭제 플래그
+                if not delflag: #왼쪽 자식노드가 또 다른 자식노드를 가지고있다는 것. 즉, 리프노드가 아니라는것
+                    #여기서 continue를 걸어버리면 오른쪽 확인을 못함
                     queue.append(node.left)
                 
-            if node.right:
-                if node.right.left == None and node.right.right == None:
-                    node.right = None 
+            if node.right: #오른쪽 자식 존재할 시
+                if node.right.left == None and node.right.right == None: #오른쪽 자식의 오른쪽이 없고 오른쪽자식의 왼쪽이 없다면
+                    node.right = None  #오른쪽 자식 삭제
                     continue #왼오 다확인했으므로 continue 걸어도됨
-                
-                queue.append(node.right)
+                #continue에 걸리지 않았다면 if문에 걸리지 않은거고 이는 곧 오른쪽자식이 리프노드가 아니라는것.
+                queue.append(node.right) 
             
     def Visualization(self, node):
-        if node.left == None and node.right == None:
+        if node.left == None and node.right == None: #왼쪽자식, 오른쪽 자식이 없다면 리프노드임
+            print(node.data,'is 리프노드')
             return
-        elif node.left != None and node.right == None:
+        elif node.left != None and node.right == None: #오른쪽 자식은 없고 왼쪽자식만 있을시...
             print(node.data,'의 자식: ',node.left.data)
             self.Visualization(node.left)
-        elif node.left == None and node.right != None:  
+        elif node.left == None and node.right != None: #왼쪽 자식은 없고 오른쪽 자식만 있을시... 
             print(node.data,'의 자식: ', node.right.data)
             self.Visualization(node.right)  
-        else:
+        else: #둘 다 자식이 있을 시...
             print(node.data,'의 자식: ',node.left.data, node.right.data)
             self.Visualization(node.left)
             self.Visualization(node.right)
+
+    def VisualizationBFS(self):
+        queue = deque()
+        queue.append(self.root)
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
             
+        
+                  
 
 start = time.time()           
 b = BinaryTree(45)
